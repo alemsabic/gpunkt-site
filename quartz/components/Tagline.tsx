bfea850d39fd17ba6a98f1b3d62d71977fc4f6d5
Alem Šabić <alemsabic@gmail.com>
2026-01-29 11:03:32 +0100

    style: additional UI and component tweaks

--- quartz/components/Tagline.tsx
+++ quartz/components/Tagline.tsx
@@ -4,14 +4,14 @@ import { classNames } from "../util/lang"
 const Tagline: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
   return (
     <div class={classNames(displayClass, "tagline", "desktop-only")}>
-      Reizwörter - Sprache ohne Filter.
+      Reizwörter - Wahrheit ist Wortwahl.
     </div>
   )
 }
 
 Tagline.css = `
 .tagline {
-  font-size: 1rem;
+  font-size: 1.2rem;
   margin-top: 1rem;
   margin-bottom: 2.5rem;
   line-height: 1.1rem;
